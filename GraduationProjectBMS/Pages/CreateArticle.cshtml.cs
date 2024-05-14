7528245e0ff6b23b0ee5626cea273ce6cb46d27d
moshoubash <ma016188@gmail.com>
2024-05-14 08:47:31 +0300

    update get started page

--- GraduationProjectBMS/Pages/CreateArticle.cshtml.cs
+++ GraduationProjectBMS/Pages/CreateArticle.cshtml.cs
@@ -1,4 +1,6 @@
+using GraduationProjectBMS.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,8 +9,17 @@ namespace GraduationProjectBMS.Pages
     [Authorize]
     public class CreateArticleModel : PageModel
     {
+        protected readonly UserManager<AppUser> userManager;
+        public AppUser? appUser;
+        public CreateArticleModel(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
         public void OnGet()
         {
+            var task = userManager.GetUserAsync(User);
+            task.Wait();
+            appUser = task.Result;
         }
     }
 }

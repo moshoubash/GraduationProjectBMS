c51e4b4f92643865b5ecc3ff62ca991129ad198c
moshoubash <ma016188@gmail.com>
2024-05-28 12:42:57 +0300

    Revert "new"
    This reverts commit cdce90610b7a814ccf2bca3fdd90762c5374ebd7.

--- GraduationProjectBMS/Models/Article.cs
+++ GraduationProjectBMS/Models/Article.cs
@@ -5,7 +5,6 @@ namespace GraduationProjectBMS.Models
     public class Article
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ArticleId { get; set; }
         public string? ArticleTitle { get; set; }
         public string? ArticleThumbnail { get; set; }
